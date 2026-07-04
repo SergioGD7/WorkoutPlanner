e920005d44e9a4359a8bcc564addbb705641d2e9
SergioGD7 <sergio.g.d7@gmail.com>
2026-07-04 22:58:13 +0200

    fix

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,7 +7,7 @@ export default function manifest(): MetadataRoute.Manifest {
     name: 'Workout Planner',
     short_name: 'Workout',
     description: 'Track your gym progress with Workout Planner.',
-    start_url: '/',
+    start_url: process.env.GITHUB_ACTIONS ? '/WorkoutPlanner/' : '/',
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#000000',
