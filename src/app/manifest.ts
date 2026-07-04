7c6c39a52f532b286a41a3b79e405c7014b65cb9
SergioGD7 <sergio.g.d7@gmail.com>
2026-07-04 23:04:33 +0200

    fix

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,17 +3,21 @@ import { MetadataRoute } from 'next';
 export const dynamic = 'force-static';
 
 export default function manifest(): MetadataRoute.Manifest {
+  const isGithub = process.env.GITHUB_ACTIONS;
+  const baseUrl = isGithub ? 'https://sergiogd7.github.io/WorkoutPlanner' : '';
+
   return {
     name: 'Workout Planner',
     short_name: 'Workout',
     description: 'Track your gym progress with Workout Planner.',
-    start_url: process.env.GITHUB_ACTIONS ? '/WorkoutPlanner/' : '/',
+    start_url: isGithub ? 'https://sergiogd7.github.io/WorkoutPlanner/' : '/',
+    scope: isGithub ? 'https://sergiogd7.github.io/WorkoutPlanner/' : '/',
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#000000',
     icons: [
       {
-        src: '/favicon.ico',
+        src: `${baseUrl}/favicon.ico`,
         sizes: 'any',
         type: 'image/x-icon',
       },
